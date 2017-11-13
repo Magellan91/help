f99392042d092bb1ef7cbba97a69a1d547879199
Magellan91 <muqpka1991@gmail.com>
2017-11-13 10:47:01 +0200

    Loader

--- js/Loader.js
+++ js/Loader.js
@@ -25,3 +25,13 @@ $(function(){
     });
   });
 });
+
+//Загрузка спомощью css
+<script>
+  $(window).on('load', function () {
+    $preloader = $('.loaderArea'),
+      $loader = $preloader.find('.loader');
+    $loader.fadeOut();
+    $preloader.delay(350).fadeOut('slow');
+  });
+</script>
